ee978c6cf7a3d89c5573df2593b0339c62afbaa7
pierre-borie <pierre.borie98@gmail.com>
2026-03-06 14:57:12 -0500

    validated tests for new model structure

--- src/model.jl
+++ src/model.jl
@@ -1,3 +1,6 @@
+# Abstract type defined just for testing
+abstract type AbstractCnlsModel{T} end
+
 """
     BoxCnls{T} <: AbstractCnlsModel{T}
 
@@ -55,14 +58,15 @@ mutable struct BoxCnls{T<:Real} <: AbstractCnlsModel{T}
     x_low::Vector{T}
     x_upp::Vector{T}
 
-    # Starting point
-    x::Vector{T}
-
     # Dimensions
     n::Int
     n_slack::Int
     m::Int
     p::Int
+
+    # Starting point
+    x::Vector{T}
+
 end
 
 # Constructor with in-place evaluation functions for a model with a mix of
@@ -92,11 +96,11 @@ function BoxCnls!(
     x_upp = vcat(upp, fill(Inf,n_slack))
 
     # Set initial slack variables to g(x₀)
-    u0 = similar(x,n_slack)
+    u0 = similar(x0,n_slack)
     g!(x0,u0)
-    x_start = vcat(x0,s0)
+    x_start = vcat(x0,u0)
 
-    return BoxCnls(r!,h!,g!,jac_r!,jac_h!,jac_g!,x_low,x_upp,x_start,n,n_slack,m,p)
+    return BoxCnls(r!,h!,g!,jac_r!,jac_h!,jac_g!,x_low,x_upp,n,n_slack,m,p,x_start)
 end
 
 # Constructor with in-place evaluation functions for a model with only
@@ -108,22 +112,28 @@ function BoxCnls!(
     jac_c!,
     low::Vector{T},
     upp::Vector{T},
-    x_start::Vector{T},
+    x0::Vector{T},
     n_var::Int,
     m::Int,
     p::Int,
     only_equalities::Bool) where T
 
     return if only_equalities
-        BoxCnls(r!,c!,nothing,jac_r!,jac_c!,nothing,low,upp,x_start,n_var,0,m,p)
+        BoxCnls(r!,c!,nothing,jac_r!,jac_c!,nothing,low,upp,n_var,0,m,p,x0)
 
     else begin
         n_slack = p
         n = n_var + n_slack
         x_low = vcat(low, zeros(n_slack))
         x_upp = vcat(upp, fill(Inf,n_slack))
-        x0 = vcat(x_start,c(x_start))
-        BoxCnls(r!,nothing,c!,jac_r!,nothing,jac_c!,x_low,x_upp,x0,n,n_slack,m,p) end
+
+        # Set initial slack variables to g(x₀)
+        u0 = similar(x0,n_slack)
+        c!(x0,u0)
+        x_start = vcat(x0,u0)
+
+        BoxCnls(r!,nothing,c!,jac_r!,nothing,jac_c!,x_low,x_upp,n,n_slack,m,p,x_start)
+    end
     end
 end
 
@@ -145,6 +155,7 @@ function BoxCnls(
     p_ineq::Int) where T
 
     # In-place versions of evaluation functions
+    println("coucou")
     function r!(x,rx)
         rx[1:m] .= r(x)
         return
@@ -192,7 +203,7 @@ function BoxCnls(
     n_var::Int,
     m::Int,
     p::Int,
-    only_equalities::Bool)
+    only_equalities::Bool) where T
 
     # In-place versions of evaluation functions
     function r!(x,rx)
@@ -211,7 +222,7 @@ function BoxCnls(
     end
 
     function jac_c!(x,Cx)
-        Hx[1:p,1:n_var] .= jac_c(x)
+        Cx[1:p,1:n_var] .= jac_c(x)
         return
     end
 
@@ -264,22 +275,22 @@ function nlconstraints!(model::BoxCnls{T}, x::Vector{T}, cx::Vector{T}) where T
 
     # Inequality constraints transformed into equalities
     if n_slack > 0
-        gxmu = view(cx, p_eq+1:p)
+        gxmu = view(cx, p_eq+1:p)    # buffer for g(x) - u
         x_slack = view(x, n_var+1:n)
-        model.nlineq!(x_var, gx) # gxmu ← g(x)
-        gxmu .-= x_slack         # gxmu ← gxmu - u
+        model.nlineq!(x_var, gxmu)   # gxmu ← g(x)
+        gxmu .-= x_slack             # gxmu ← gxmu - u
     end
 
     return
 end
 
 """
-    nlconstraints(model::BoxCnls, x::Vector)
+    nlconstraints(model, x)
 
 Returns the nonlinear constraints for the given `model` evaluated at input vector
 `x`.
 """
-function nlconstraints(model::BoxCnls,x::Vector)
+function nlconstraints(model::BoxCnls{T}, x::Vector{T}) where T
     cx = similar(x, model.p)
     nlconstraints!(model, x, cx)
     return cx
@@ -291,13 +302,13 @@ end
 Evaluates the Jacobian of the residuals for the given `model` at input vector `x`,
 storing the result in matrix `J`.
 """
-function jac_residuals!(model::BoxCnls, x::Vector, J::Matrix)
+function jac_residuals!(model::BoxCnls{T}, x::Vector{T}, J::Matrix{T}) where T
     n, n_slack, m = model.n, model.n_slack, model.m
     n_var = n - n_slack
     x_var = view(x,1:n_var)
 
     # Derivatives with respect to decision variables
-    Jxvar = view(J, 1:m, 1:n)
+    Jxvar = view(J, 1:m, 1:n_var)
     model.jac_res!(x_var, Jxvar)
 
     # Derivatives with respect to slack variables
@@ -372,3 +383,304 @@ function jac_nlconstraints(model::BoxCnls{T},x::Vector{T}) where T
 end
 
 # Testing
+using Test, LinearAlgebra, ForwardDiff
+
+
+@testset "Model with in-place evaluation functions and inequality constraints" begin
+    # Problem 65 form Hock-Schittkowski
+    n = 3
+    m = 3
+    p = 1
+
+    # Residuals
+    r(x) = [x[1]-x[2],
+    (x[1]+x[2]-10)/3,
+    x[3]-5.0]
+
+    function r!(x,rx)
+        rx .= [x[1]-x[2],
+               (x[1]+x[2]-10)/3,
+               x[3]-5.0]
+        return
+    end
+
+    jac_r(x) = [1. -1. 0;
+        1/3 1/3 0.;
+        0. 0. 1.;]
+
+    function jac_r!(x,J)
+        J .= [1. -1. 0;
+        1/3 1/3 0.;
+        0. 0. 1.;]
+        return
+    end
+    # Equality constraints
+    c(x) = [48.0 - x[1]^2 - x[2]^2 - x[3]^2]
+    jac_c(x) = [-2x[1] -2x[2] -2x[3]]
+
+    function c!(x,cx)
+        cx .= [48.0 - x[1]^2 - x[2]^2 - x[3]^2]
+        return
+    end
+
+    function jac_c!(x,C)
+        C .= [-2x[1] -2x[2] -2x[3]]
+        return
+    end
+
+    # Bounds
+    x_low = [-4.5, -4.5, -5.0]
+    x_upp = [4.5, 4.5, 5.0]
+
+    x0 = [-5, 5, 0.0]
+    x = vcat(x0,c(x0))
+
+    # Testing for model defined with in-place methods
+    model = BoxCnls!(r!,c!,jac_r!,jac_c!,x_low,x_upp,x0,n,m,p,false)
+
+    @test size(x,1) == model.n
+    @test residuals(model,x) ≈ r(x0)
+    @test model.n_slack == p
+    @test jac_residuals(model,x) ≈ [1. -1. 0 0;
+        1/3 1/3 0. 0;
+        0. 0. 1. 0;]
+    @test jac_nlconstraints(model,x) ≈ hcat(jac_c(x0),[-1])
+    @test nlconstraints(model,x) ≈ zeros(1)
+    @test model.nleq! === nothing && model.jac_nleq! === nothing
+
+    # Testing for model defined with standard functions
+    model = BoxCnls(r,c,jac_r,jac_c,x_low,x_upp,x0,n,m,p,false)
+
+    @test size(x,1) == model.n
+    @test residuals(model,x) ≈ r(x0)
+    @test model.n_slack == p
+    @test jac_residuals(model,x) ≈ [1. -1. 0 0;
+        1/3 1/3 0. 0;
+        0. 0. 1. 0;]
+    @test jac_nlconstraints(model,x) ≈ hcat(jac_c(x0),[-1])
+    @test nlconstraints(model,x) ≈ zeros(1)
+    @test model.nleq! === nothing && model.jac_nleq! === nothing
+end
+
+@testset "Model with only equality constraints" begin
+    n = 10
+    m = 2(n-1)
+    p = n-2
+
+    # Residuals
+
+    function r(x)
+        n = length(x)
+        m = 2(n-1)
+        rx = Vector{eltype(x)}(undef,m)
+        rx[1:n-1] = [10(x[i]^2 - x[i+1]) for i=1:n-1]
+        rx[n:m] = [x[k-n+1] - 1 for k=n:m]
+        return rx
+    end
+
+    function r!(x,rx)
+        n = size(x,1)
+        m = 2(n-1)
+
+        rx[1:n-1] .= [10(x[i]^2 - x[i+1]) for i=1:n-1]
+        rx[n:m] .= [x[k-n+1] - 1 for k=n:m]
+        return
+    end
+
+    function jac_r(x)
+        n = size(x,1)
+        m = 2(n-1)
+        J = zeros(eltype(x), (m,n))
+
+        for i=1:n-1
+            J[i,i] = 20x[i]
+            J[i,i+1] = -10
+        end
+
+        for i=n:m
+            J[i,i-n+1] = 1
+        end
+        return J
+    end
+
+    function jac_r!(x, J)
+        n = size(x,1)
+        m = 2(n-1)
+
+        for i=1:n-1
+            J[i,i] = 20x[i]
+            J[i,i+1] = -10
+        end
+
+        for i=n:m
+            J[i,i-n+1] = 1
+        end
+        return
+    end
+
+    # Constraints
+
+    function c(x)
+        n = length(x)
+        cx = [3x[k+1]^3 + 2x[k+2] - 5 + sin(x[k+1]-x[k+2])*sin(x[k+1]+x[k+2]) + 4x[k+1] -
+            x[k]*exp(x[k]-x[k+1]) - 3 for k=1:n-2]
+        return cx
+    end
+
+    function c!(x, cx)
+        n = length(x)
+        cx .= [3x[k+1]^3 + 2x[k+2] - 5 + sin(x[k+1]-x[k+2])*sin(x[k+1]+x[k+2]) + 4x[k+1] -
+            x[k]*exp(x[k]-x[k+1]) - 3 for k=1:n-2]
+        return cx
+    end
+
+    function jac_c(x)
+        n = size(x,1)
+        A = zeros(eltype(x), (n-2,n))
+        for k=1:n-2
+            A[k,k] = -(x[k]+1) * exp(x[k]-x[k+1])
+            A[k,k+1] = 9x[k+1]^2 + cos(x[k+1]-x[k+2])*sin(x[k+1]+x[k+2]) + sin(x[k+1]-x[k+2])*cos(x[k+1]+x[k+2]) + 4 + x[k]*exp(x[k]-x[k+1])
+            A[k,k+2] = 2 - cos(x[k+1]-x[k+2])*sin(x[k+1]+x[k+2]) + sin(x[k+1]-x[k+2])*cos(x[k+1]+x[k+2])
+        end
+        return A
+    end
+
+    function jac_c!(x,A)
+        n = size(x,1)
+        for k=1:n-2
+            A[k,k] = -(x[k]+1) * exp(x[k]-x[k+1])
+            A[k,k+1] = 9x[k+1]^2 + cos(x[k+1]-x[k+2])*sin(x[k+1]+x[k+2]) + sin(x[k+1]-x[k+2])*cos(x[k+1]+x[k+2]) + 4 + x[k]*exp(x[k]-x[k+1])
+            A[k,k+2] = 2 - cos(x[k+1]-x[k+2])*sin(x[k+1]+x[k+2]) + sin(x[k+1]-x[k+2])*cos(x[k+1]+x[k+2])
+        end
+        return
+    end
+
+    x_low = fill(-Inf,n)
+    x_upp = fill(Inf,n)
+
+
+    # Starting point
+    x0 = [(mod(i,2) == 1 ? -1.2 : 1.0) for i=1:n]
+
+    model = BoxCnls!(r!,c!,jac_r!,jac_c!,x_low,x_upp,x0,n,m,p,true)
+    @test size(x0,1) == model.n
+    @test model.n_slack == 0
+    @test model.p == p
+    @test residuals(model,x0) ≈ r(x0)
+    @test jac_residuals(model,x0) ≈ jac_r(x0)
+    @test jac_nlconstraints(model,x0) ≈ jac_c(x0)
+    @test nlconstraints(model,x0) ≈ c(x0)
+    @test model.nlineq! === nothing && model.jac_nlineq! === nothing
+
+    # Testing for model defined with standard functions
+    model = BoxCnls(r,c,jac_r,jac_c,x_low,x_upp,x0,n,m,p,true)
+
+    @test size(x0,1) == model.n
+    @test model.n_slack == 0
+    @test model.p == p
+    @test residuals(model,x0) ≈ r(x0)
+    @test jac_residuals(model,x0) ≈ jac_r(x0)
+    @test jac_nlconstraints(model,x0) ≈ jac_c(x0)
+    @test nlconstraints(model,x0) ≈ c(x0)
+    @test model.nlineq! === nothing && model.jac_nlineq! === nothing
+end
+
+@testset "Model with a mix of equalities and inequalies" begin
+
+    n = 5
+    m = 4
+    p_eq = 1
+    p_ineq = 1
+
+    # Residuals
+
+    r(x) = [exp((x[i]-i)^2 / 2x[i+1]^2) for i=1:n-1]
+
+    function r!(x,rx)
+        rx .= [exp((x[i]-i)^2 / 2x[i+1]^2) - i for i=1:n-1]
+    end
+
+    function jac_r(x)
+        n = size(x,1)
+        J = zeros(n-1,n)
+
+        for i=1:n-1
+            J[i,i] = 2x[i] * exp((x[i]-i)^2 / 2x[i+1]^2)
+            J[i,i+1] = exp((x[i]-i)^2 / 2x[i+1]^2) / x[i+1]^3
+        end
+
+        return J
+    end
+
+    function jac_r!(x,J)
+        J .= 0
+        for i=1:size(x,1)-1
+            J[i,i] = 2x[i] * exp((x[i]-i)^2 / 2x[i+1]^2)
+            J[i,i+1] = exp((x[i]-i)^2 / 2x[i+1]^2) / x[i+1]^3
+        end
+        return
+    end
+
+    # Constraints
+
+    g(x) = [1 - x[1]^2 - x[2]^2 - x[3]^2]
+
+    function g!(x,gx)
+        gx .= [1 - x[1]^2 - x[2]^2 - x[3]^2]
+        return
+    end
+
+    jac_g(x) =  [-2x[1] -2x[2] -2x[3] 0.0 0.0]
+
+    function jac_g!(x,Gx)
+        Gx .= [-2x[1] -2x[2] -2x[3] 0.0 0.0]
+    end
+
+    h(x) = [x[4]^2 + x[5]^2 - 1]
+
+    function h!(x,hx)
+        hx .= [x[4]^2 + x[5]^2 - 1]
+        return
+    end
+
+    jac_h(x) = [0.0 0.0 0.0 2x[4] 2x[5]]
+
+    function jac_h!(x,Hx)
+        Hx .= [0.0 0.0 0.0 2x[4] 2x[5]]
+        return
+    end
+
+    x_low = [0.1^i for i=1:n]
+    x_upp = [Float64(i^2) for i=1:n]
+
+    # Starting point
+    x0 = [1/i for i=1:n]
+
+    # Model with in place methods
+    model = BoxCnls!(r!,h!,g!,jac_r!,jac_h!,jac_g!,x_low,x_upp,x0,n,m,p_eq,p_ineq)
+
+    @test n+p_ineq == model.n
+    @test model.n_slack == 1
+    @test model.p == p_eq+p_ineq
+    @test size(model.x,1) == model.n
+    @test residuals(model,model.x) ≈ r(x0)
+    @test jac_residuals(model,model.x) ≈ hcat(jac_r(x0),zeros(m))
+    @test nlconstraints(model,model.x) ≈ vcat(h(x0),g(x0)-g(x0))
+    @test jac_nlconstraints(model,model.x) ≈ vcat(hcat(jac_h(x0),zeros(1,p_ineq)),
+                                                  hcat(jac_g(x0), Matrix{Float64}(-I,p_ineq,p_ineq)))
+
+    # Model with out of place methods
+    model = BoxCnls(r,h,g,jac_r,jac_h,jac_g,x_low,x_upp,x0,n,m,p_eq,p_ineq)
+
+    @test n+p_ineq == model.n
+    @test model.n_slack == 1
+    @test model.p == p_eq+p_ineq
+    @test size(model.x,1) == model.n
+    @test residuals(model,model.x) ≈ r(x0)
+    @test jac_residuals(model,model.x) ≈ hcat(jac_r(x0),zeros(m))
+    @test nlconstraints(model,model.x) ≈ vcat(h(x0),g(x0)-g(x0))
+    @test jac_nlconstraints(model,model.x) ≈ vcat(hcat(jac_h(x0),zeros(1,p_ineq)),
+                                                  hcat(jac_g(x0), Matrix{Float64}(-I,p_ineq,p_ineq)))
+
+
+end
