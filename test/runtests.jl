5fe22438a0c2919669fde9111c8fda1841c1a723
pierre-borie <pierre.borie98@gmail.com>
2026-05-01 17:45:47 -0400

    rearrange tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,10 @@ using Traulls, LinearAlgebra, ForwardDiff
 using Test
 
 
-# include("aug_lag.jl")
-# include("projection.jl")
-# include("hessian.jl")
-# include("workspace.jl")
-# include("metrics.jl")
-# include("model.jl")
+include("aug_lag.jl")
+include("projection.jl")
+include("hessian.jl")
+include("workspace.jl")
+include("metrics.jl")
+include("model.jl")
 include("solve_hs65.jl")
